1abdf8f11de0dfb14bbf99db095458d13dc78eae
abdallah-alabed <abd.alabed97@gmail.com>
2021-07-26 14:58:57 +0300

    about me game

---
+++ js/app.js
@@ -0,0 +1,44 @@
+'use strict'
+let first=prompt('what is your name?')
+
+alert('welcome to our website '+ first +'hopefully you will like our website!')
+alert('so me and you '+first+'can connect more i suggest we play a game!')
+
+// question 1
+let qone=prompt('guess the following: "my favourite animals are Bears"   (yes/y no/n)'). toLowerCase();
+if (qone == 'yes' || qone == 'y'){
+    alert('correct, i love how big and strong they are!')
+    // console.log(1st question guessed correctly)
+} 
+
+// question 2
+let qtwo=prompt('guess the following: "i cheer manchester united as my soccer team"   (yes/y no/n)'). toLowerCase();
+if (qtwo == 'no' || qtwo == 'n'){
+    alert('I have always been a huge fan for Real madrid !')
+    // console.log(2nd question guessed correctly)
+} 
+
+// question 3
+let qthree=prompt('guess the following: "i drive a BMW"   (yes/y no/n)'). toLowerCase();
+if (qthree == 'no' || qthree == 'n'){
+    alert('I actually like compact cars thats why i have a Golf!')
+    // console.log(3rd question guessed correctly)
+} 
+
+// question 4
+let qfour=prompt('guess the following: "i drive a BMW"   (yes/y no/n)'). toLowerCase();
+if (qfour == 'no' || qfour == 'n'){
+    alert('I actually like compact cars thats why i have a Golf!')
+    // console.log(4th question guessed correctly)
+} 
+
+
+// question 5
+let qfive=prompt('guess the following: "spongebob is the greatest cartoon of all time"   (yes/y no/n)'). toLowerCase();
+if (qfive == 'yes' || qfive == 'y'){
+    alert('ayy ayy captain!')
+    // console.log(5th question guessed correctly)
+} 
+
+
+alert('thank you '+ first+'for using our website hopefully you will have a wonderful day today and we cant wait to see you again!')
